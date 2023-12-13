5b50ebc25a566bb539ba38ab275499f565c6a1bf
MeganKR <megankaye2018@gmail.com>
2023-12-12 19:03:33 -0600

    added basic controllers

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again.' });
+            return;
+        }
+    }
+})
+
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
