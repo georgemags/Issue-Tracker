4dfa54e2b2a655d76a529c10d2208f0550ece122
ellysaschottler <ellysastork@gmail.com>
2023-12-18 17:10:19 -0600

    fixed render connection info

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.POSTGRES_DB) {
-  sequelize = new Sequelize("postgres://issue_tracker_postgres_db_user:POFbKzVO1H7HuwDuF3TXXwt0GtTTGty6@dpg-cltrs7i1hbls73e6feg0-a.ohio-postgres.render.com/issue_tracker_postgres_db?ssl=true");
+  sequelize = new Sequelize(process.env.POSTGRES_DB);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
